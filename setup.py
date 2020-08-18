57936764bc429ed1f3650fc57cb636a8dc47aa01
Alin Voinea <contact@avoinea.com>
2020-08-18 10:18:57 +0300

    "Create addon: eea.zotero"

---
+++ setup.py
@@ -0,0 +1,54 @@
+""" eea.zotero Installer
+"""
+import os
+from os.path import join
+from setuptools import setup, find_packages
+
+NAME = 'eea.zotero'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description="Plone RestAPI endpoint for Zotero",
+      long_description=(
+          open("README.rst").read() + "\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      ),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: Addon",
+          "Framework :: EEA :: Addon",
+          "Framework :: Plone :: 5.2",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+      ],
+      keywords='EEA Add-ons Plone Zope',
+      author='European Environment Agency: IDM2 A-Team',
+      author_email='eea-edw-a-team-alerts@googlegroups.com',
+      url='https://github.com/eea/eea.zotero',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+            python_requires="==2.7, >=3.6",
+            install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )

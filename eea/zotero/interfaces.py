1abfd22ea5806358ee0bcf42ca6523d45fa5204d
Mihai Dobrescu <151914606+mihaidobrescu1111@users.noreply.github.com>
2025-11-18 13:44:32 +0200

    tests: Replace all python linters to ruff - refs #286821

--- eea/zotero/interfaces.py
+++ eea/zotero/interfaces.py
@@ -11,29 +11,28 @@ class IEeaZoteroLayer(IDefaultBrowserLayer):
 
 
 class IZoteroClientSettings(Interface):
-    """ Client settings for Zotero
-    """
+    """Client settings for Zotero"""
+
     server = schema.TextLine(
-        title=_(u"Zotero API URL"),
-        description=_(u"Zotero API URL including user/group id"),
-        default=u"https://api.zotero.org/users/12345"
+        title=_("Zotero API URL"),
+        description=_("Zotero API URL including user/group id"),
+        default="https://api.zotero.org/users/12345",
     )
 
     password = schema.TextLine(
-        title=_(u"Zotero API KEY"),
-        description=(u"Zotero API KEY with read/write permissions"),
-        default=u""
+        title=_("Zotero API KEY"),
+        description=("Zotero API KEY with read/write permissions"),
+        default="",
     )
 
     default = schema.TextLine(
-        title=_(u"Zotero default collection"),
-        description=(u"Zotero collection id where to store new citations"),
-        default=u""
+        title=_("Zotero default collection"),
+        description=("Zotero collection id where to store new citations"),
+        default="",
     )
 
     style = schema.TextLine(
-        title=_(u"Zotero citation style"),
-        description=_(
-            "Zotero citation style, e.g.: oxford or an URL to a .csl file"),
-        default=u"https://www.eea.europa.eu/zotero/eea.csl"
+        title=_("Zotero citation style"),
+        description=_("Zotero citation style, e.g.: oxford or an URL to a .csl file"),
+        default="https://www.eea.europa.eu/zotero/eea.csl",
     )
